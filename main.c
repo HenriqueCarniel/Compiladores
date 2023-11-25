3041a2fbf0e03496aa1116a5c76beb688aa7e6de
HenriqueCarniel <henriquecarniel80@gmail.com>
2023-11-25 06:27:59 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,13 @@
-/*
-Função principal para realização da análise sintática.
-
-Este arquivo será posterioremente substituído, não acrescente nada.
-*/
 #include <stdio.h>
-#include "parser.tab.h" //arquivo gerado com bison -d parser.y
+extern int yyparse(void);
 extern int yylex_destroy(void);
-extern int get_line_number (void);
-
-extern int yyerror(const char* mensagem){
-  printf("Erro na linha %d : %s\n", get_line_number(), mensagem);
-}
-
+void *arvore = NULL;
+void exporta (void *arvore);
 
 int main (int argc, char **argv)
 {
-  int ret = yyparse();
+  int ret = yyparse(); 
+  exporta (arvore);
   yylex_destroy();
   return ret;
 }
