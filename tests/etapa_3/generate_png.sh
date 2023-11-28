a2a14a2d600554d37ae63c37684293a21195a117
HenriqueCarniel <henriquecarniel80@gmail.com>
2023-11-28 19:16:56 -0300

    Changing file locations

--- tests/etapa_3/generate_png.sh
+++ tests/etapa_3/generate_png.sh
@@ -1,3 +1,3 @@
 ./etapa3 < tests/etapa_3/input/w$1 > tests/etapa_3/output/saida
-./output2dot.sh < tests/etapa_3/output/saida > tests/etapa_3/output/saida.dot
+tests/etapa_3/output2dot.sh < tests/etapa_3/output/saida > tests/etapa_3/output/saida.dot
 dot -Tpng tests/etapa_3/output/saida.dot -o tests/etapa_3/output/saida.png
